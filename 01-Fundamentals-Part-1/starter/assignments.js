d2dffa3aba5791a862d6d45bb9267d1ee1d8a1b8
rogueassassin11 <maecagadas2011@gmail.com>
2024-01-03 22:30:41 +0800

    Done until Basic Operators

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,49 @@
+////////////////////////////////////////
+// VALUES AND VARIABLES
+/*
+let country = "Shire";
+let continent = "Middle-earth";
+let population = 300;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+////////////////////////////////////////
+// DATA TYPES
+/*
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(language);
+*/
+
+////////////////////////////////////////
+// LET, CONST, VAR
+/*
+let language;
+language = "Shire Common Tongue";
+
+const country = "Shire";
+const continent = "Middle-earth";
+let population = 300;
+const isIsland = false;
+*/
+
+////////////////////////////////////////
+// BASIC OPERATORS
+/*
+let population = 300;
+console.log(population / 2);
+console.log(population++);
+console.log(population > 6000000);
+console.log(population < 33000000);
+let description =
+  "Shire is in Middle-earth, and its" +
+  " " +
+  population +
+  " Hobbits speak the Shire Common Tongue.";
+console.log(description);
+*/
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,109 @@
+////////////////////////////////////////
+// VARIABLES
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Elias");
+console.log("11");
+
+let firstName = "Elias";
+console.log(firstName);
+
+//variables cannot start with a number, only numbers, letters, underscore, and $ sign
+//let 3years = 3;
+//let jonas&matilda
+// do not start with a capital letter
+// constant numbers are in uppercase ->
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+console.log(myFirstJob);
+*/
+
+////////////////////////////////////////
+// DATA TYPES
+/*
+values can be an object or a primitive
+Dynamic typing = data types are determined automatically; the value has the type not the variable
+Data Types:
+Number, String, Boolean, Undefined, Null, Symbol, BigInt */
+/*
+// Boolean
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Elias");
+
+// Dynamic Typing
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+// Undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2070;
+console.log(typeof year);
+
+// Typeof ERROR
+console.log(typeof null);
+*/
+
+////////////////////////////////////////
+// LET, CONST, VAR
+// let -> when you want to mutate and be empty initially
+// const -> value cannot be changed, should always be initialized
+// var -> old way of defining a variable prior to ES6
+/*
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+var job = "programmer";
+job = "teacher";
+console.log(job);
+*/
+
+////////////////////////////////////////
+// BASIC OPERATORS
+/*
+//Math operators
+const now = 2200;
+const ageElias = now - 1991;
+const ageChise = now - 2050;
+
+console.log(ageElias, ageChise);
+console.log(ageElias * 2, ageElias / 2);
+
+//EXPONENT
+console.log(2 ** 3);
+
+//CONCATENATE STRINGS
+const firstName = "Elias";
+const lastName = "Ainsworth";
+console.log(firstName + " " + lastName);
+
+//ASSIGNMENT OPERATORS
+let x = 10 + 5; // 15
+x += 10; // x = x + 10
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1;
+x--;
+x--;
+console.log(x);
+
+// Comparison Operators
+// >, <, >=, <=
+console.log(ageElias > ageChise);
+console.log(ageChise >= 18);
+
+const isFullAge = ageChise >= 18;
+*/
