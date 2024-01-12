1115b52c488f779bc03dafd5e3389712e2f35e02
rogueassassin11 <maecagadas2011@gmail.com>
2024-01-12 16:00:32 +0800

    Until How to Think Like a Developer

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,41 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+/////////////////////////////////////////
+// SETTING UP VS CODE
+/*
+1. Prettier - opinionated formatter
+  - default formatter -> esbenp
+  - format on save -> check the box
+
+2. Snippets
+  - preferences - user code snippets
+  - 'cl' to expand console.log()
+
+3. Auto Rename
+4. Auto Close
+5. Image Preview
+5. TODO Highlight
+6. LiveServer Extension
+7. NodeJS
+  - npm install live-server -g
+
+
+const x = '23';
+
+const calcAge = (birthYear) => 2037 - birthYear;
+console.log();
+*/
+
+/////////////////////////////////////////
+// LEARNING HOW TO CODE
+/*
+
+4 STEPS TO SOLVE ANY PROBLEM
+1. Ask the right questions to get a clear picture of the problem. Understand it completely.
+2. Divide and Conquer: break a big problem into smaller sub-problems.
+3. Don't be afraid to research.
+4. Write a pseudo-code before writing the actual code.
+
+
+*/
