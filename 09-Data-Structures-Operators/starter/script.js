ba1bc5c269066c686132159be26900a7998bb089
rogueassassin11 <maecagadas2011@gmail.com>
2024-01-23 22:03:05 +0800

    Until Destructuring Objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,115 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  // destructuring object arguments
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    address,
+    time = '20:00',
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
+
+/*
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+
+  starterIndex: 1,
+});
+*/
+
+/////////////////////////////////////////
+// DESTRUCTURING OBJECTS
+/*
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+// changing object property names
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// setting default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// mutating variables by wrapping in parenthesis
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+*/
+
+/////////////////////////////////////////
+// ARRAY DESTRUCTURING
+/*
+// without destructuring
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+// with destructuring
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [first, second] = restaurant.categories;
+console.log(first, second);
+
+// to skip
+const [first1, , second1] = restaurant.categories;
+console.log(first1, second1);
+
+// to switch variables
+const temp = first;
+first = second;
+second = temp;
+console.log(first, second);
+
+[first, second] = [second, first];
+console.log(first, second);
+
+// working with methods
+console.log(restaurant.order(2, 0));
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+//nested arrays
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j);
+
+// destructuring inside destructuring
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// setting default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+*/
